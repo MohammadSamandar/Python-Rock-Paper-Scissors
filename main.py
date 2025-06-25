6801c0e3dfb45dfe00b265dcbdaa0c7de51b9cc0
mohammad <m.samandarhd@gmail.com>
2025-06-25 15:59:10 +0330

    refactor: Structure the game logic into functions

--- main.py
+++ main.py
@@ -2,13 +2,9 @@ from random import choice
 
 
 
+options = ['rock', 'paper', 'scissors']
 
-while True:
-
-
-    options = ['rock', 'paper', 'scissors']
-
-
+def get_user_choice():
     while True:
         userinput = input("Select an option {rock, paper, scissors}: ").lower()
         if userinput in options:
@@ -18,31 +14,124 @@ while True:
     
 
     print("user selected: ", userinput)
+    return userinput
+
+
+
+
+
+def get_computer_choice():
 
     computer_choice = choice(options)
     print("bot selected: ", computer_choice)
+    return computer_choice
 
-    if userinput == computer_choice:
-        print("It's a tie!")
 
-    elif (userinput == 'rock' and computer_choice == 'scissors') or (userinput == 'paper' and       computer_choice == 'rock') or (userinput == 'scissors' and computer_choice == 'paper'):
-        print("You won!")
+
+
+
+def determine_winner(player_choice, computer_choice):
+
+
+    if player_choice == computer_choice:
+        return "tie"
+
+    elif (player_choice == 'rock' and computer_choice == 'scissors') or (player_choice == 'paper' and       computer_choice == 'rock') or (player_choice == 'scissors' and computer_choice == 'paper'):
+        return "win"
 
 
     else:
 
-        print('You Failed!!!')
+        return "lose"
 
 
+
+
+
+
+def play_game():
 
     while True:
-    
-        play_again = input("Do you want play again? {yes , no}: ").lower()
-        play_again_list = ['yes', 'no']
-        if play_again in play_again_list:
-            break
+        
+        
+        user_choice = get_user_choice()
+        computer_choice = get_computer_choice()
+        game_result  = determine_winner(user_choice,computer_choice)
+        if game_result  == "tie":
+            print("It's a tie")
+        elif game_result  == "lose":
+            print("You Failed!!!")
         else:
-            print("Invaid Input.")
+            print("You won!")
+    
 
-    if play_again == 'no':
-        break
+            
+
+
+        while True:
+    
+            play_again = input("Do you want play again? {yes , no}: ").lower()
+            play_again_list = ['yes', 'no']
+            if play_again in play_again_list:
+                break
+            else:
+                print("Invaid Input.")
+
+        if play_again == 'no':
+            break
+
+
+if __name__ == "__main__":
+    play_game()
+
+
+
+# while True:
+
+
+    # options = ['rock', 'paper', 'scissors']
+
+
+    # while True:
+    #     userinput = input("Select an option {rock, paper, scissors}: ").lower()
+    #     if userinput in options:
+    #         break
+    #     else: 
+    #         print("Invalid Input")
+    
+
+    # print("user selected: ", userinput)
+
+
+
+    # computer_choice = choice(options)
+    # print("bot selected: ", computer_choice)
+
+
+
+
+
+    # if userinput == computer_choice:
+    #     print("It's a tie!")
+
+    # elif (userinput == 'rock' and computer_choice == 'scissors') or (userinput == 'paper' and       computer_choice == 'rock') or (userinput == 'scissors' and computer_choice == 'paper'):
+    #     print("You won!")
+
+
+    # else:
+
+    #     print('You Failed!!!')
+
+
+
+    # while True:
+    
+    #     play_again = input("Do you want play again? {yes , no}: ").lower()
+    #     play_again_list = ['yes', 'no']
+    #     if play_again in play_again_list:
+    #         break
+    #     else:
+    #         print("Invaid Input.")
+
+    # if play_again == 'no':
+    #     break
